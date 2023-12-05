469256ab19c7468eba65749066f44498fc9eb17e
BraianRiico <daviidriico@gmail.com>
2023-12-05 01:28:36 -0500

    Caso de uso para eliminar una comunidad

--- resources/views/livewire/dashboard/communities/actions/communities-table-actions.blade.php
+++ resources/views/livewire/dashboard/communities/actions/communities-table-actions.blade.php
@@ -1,4 +1,4 @@
 <div class="flex justify-end items-center space-x-2">
-    <a href="" class="mr-1 cursor-pointer hover:text-project-error disabled"><x-icons.delete-trash></x-icons.delete-trash></a>
+    <a href="{{route('dashboard.communities.delete', ['id' => $id])}}" class="mr-1 cursor-pointer hover:text-project-error"><x-icons.delete-trash></x-icons.delete-trash></a>
     <a onclick="scrollToBottom();" wire:click="$emit('update-community', {{ $id }})" class="mr-1 cursor-pointer hover:text-project-icon-hover"><x-icons.edit-pencil></x-icons.edit-pencil></a>
 </div>
