2071f9c7719dfb8b567b8ad564e4dbeee42dc056
BraianRiico <daviidriico@gmail.com>
2023-12-26 00:25:14 -0500

    Actualización de tipos de programas y eliminando el campo 'otro cual' del formulario

--- database/seeders/TypeProgramSeeder.php
+++ database/seeders/TypeProgramSeeder.php
@@ -16,11 +16,14 @@ class TypeProgramSeeder extends Seeder
     {
         $data = [
             ['id' => '1', 'name' => 'Guardianes de la Naturaleza'],
-            ['id' => '2', 'name' => 'Ecoescuelas'],
-            ['id' => '3', 'name' => 'Hogares Ecológicos'],
-            ['id' => '4', 'name' => 'Piragüa'],
-            ['id' => '5', 'name' => 'Mercados verdes'],
-            ['id' => '6', 'name' => 'Otros. Cuales'],
+            ['id' => '2', 'name' => 'Mujeres tierra y vida'],
+            ['id' => '3', 'name' => 'pagos por servicios ambientales'],
+            ['id' => '4', 'name' => 'Ecoescuelas'],
+            ['id' => '5', 'name' => 'Hogares Ecológicos'],
+            ['id' => '6', 'name' => 'Mercados verdes'],
+            ['id' => '7', 'name' => 'Piragüa'],
+            ['id' => '8', 'name' => 'procesos de investigación para el fortalecimiento de saberes ancestrales y culturales'],
+            ['id' => '9', 'name' => 'Acciones para la gestión de riesgos y la adaptación al cambio climático'],
         ];
 
         TypeProgram::upsert($data, ['id'], ['name']);
