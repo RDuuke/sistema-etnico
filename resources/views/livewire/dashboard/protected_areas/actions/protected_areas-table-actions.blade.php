70485c410fd5b3a9ed55de26e4492d38254b9297
BraianRiico <daviidriico@gmail.com>
2023-12-26 02:25:59 -0500

    Servicio para eliminar un área protegida

--- resources/views/livewire/dashboard/protected_areas/actions/protected_areas-table-actions.blade.php
+++ resources/views/livewire/dashboard/protected_areas/actions/protected_areas-table-actions.blade.php
@@ -1,4 +1,4 @@
 <div class="flex justify-end items-center space-x-2">
-    <a href="{{ route('dashboard.protected-areas.delete', [ 'community_id' => $community_id, 'id' => $id ]) }}" class="mr-1 hover:text-project-error disabled"><x-icons.delete-trash></x-icons.delete-trash></a>
+    <a href="{{ route('dashboard.protected-areas.delete', [ 'community_id' => $community_id, 'id' => $id ]) }}" class="mr-1 hover:text-project-error"><x-icons.delete-trash></x-icons.delete-trash></a>
     <a href="{{ route('dashboard.protected-areas.edit', [ 'community_id' => $community_id, 'id' => $id ]) }}" class="mr-1 hover:text-project-icon-hover"><x-icons.edit-pencil></x-icons.edit-pencil></a>
 </div>
