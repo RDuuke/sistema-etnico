c7e7cbb674cf766c369bdb671493c611e35e3ea4
Juan Duque <juan.duque@csiete.org>
2023-12-26 21:22:28 -0500

    Agregando migracion

---
+++ database/migrations/2023_12_27_014230_change_occupied_area_type_in_communities_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('communities', function (Blueprint $table) {
+            DB::statement('ALTER TABLE communities ALTER COLUMN occupied_area TYPE integer USING (occupied_area::integer)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('communities', function (Blueprint $table) {
+            $table->string('occupied_area')->change();
+        });
+    }
+};
