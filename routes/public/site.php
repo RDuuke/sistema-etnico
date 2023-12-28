99cdb728db6f451520d058e485781c69790cd4d5
Juan Duque <juuanduuke@gmail.com>
2023-12-27 21:03:24 -0500

    agregando nueva ruta

--- routes/public/site.php
+++ routes/public/site.php
@@ -4,6 +4,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Public\HomeController;
+use App\Http\Controllers\Public\GetCommunityByIdController;
 
 
 
@@ -11,3 +12,4 @@ Route::get('/', HomeController::class)->name('home');
 Route::view('/natives', 'publics.natives')->name('natives');
 Route::view('/afrocolombianos', 'publics.afrocolombianos')->name('afrocolombianos');
 Route::view('/geographical-portal', 'publics.geographical-portal')->name('geographical-portal');
+Route::get('/community/{id}', GetCommunityByIdController::class)->name('community');
